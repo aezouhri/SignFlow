93ce2e48818c1777e169a2631fcbc4a2be8f8c48
Adnane Ezouhri <adnane-ezouhri@uiowa.edu>
2023-09-23 14:25:49 -0500

    Was able to load the model on webpage.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
 // Create a scene
 const scene = new THREE.Scene();
 
@@ -11,11 +13,24 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// Create a group for the model and lights
+const group = new THREE.Group();
+scene.add(group);
+
 // Load the GLB model
-const loader = new THREE.GLTFLoader();
+const loader = new GLTFLoader();
 loader.load('hand_animated_v6.glb', function (gltf) {
     const model = gltf.scene;
-    scene.add(model);
+    model.scale.set(0.5, 0.5, 0.5);
+    group.add(model);
+
+    // Add lighting to the model
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // color, intensity
+    model.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // color, intensity
+    directionalLight.position.set(1, 1, 1); // set the position of the light
+    model.add(directionalLight);
 });
 
 // Add animation or other logic here
